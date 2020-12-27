962e1875ab33f9f8d47abcee31848a74ceddcd48
Abdulaziz Ladan <abdulazizladan@gmail.com>
2020-12-27 23:01:59 +0100

    feat: Implement site view

--- src/app/feature/site/components/sites/sites.component.ts
+++ src/app/feature/site/components/sites/sites.component.ts
@@ -1,12 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSort, MatTableDataSource } from '@angular/material';
 import { SitesService } from '../../services/sites.service';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-sites',
   templateUrl: './sites.component.html',
-  styleUrls: ['./sites.component.scss']
+  styleUrls: ['./sites.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class SitesComponent implements OnInit {
+
+  displayedColumns: string[] = ['name', 'state', 'repair_cost'];
+  dataSource: MatTableDataSource<any>;
+  expandedElement: PeriodicElement | null;
 
   constructor( private siteService : SitesService){ 
     this.getSites();
@@ -15,7 +28,7 @@ export class SitesComponent implements OnInit {
   getSites(){
     this.siteService.getSites().subscribe(
       res => {
-        console.log(res)
+        this.dataSource = new MatTableDataSource(res);
       },
       err => {
         console.log("Could not fetch resource")
@@ -25,6 +38,10 @@ export class SitesComponent implements OnInit {
 
   ngOnInit(): void {
   
+  }
+
+  applyFilter(filterValue : string){
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
--- src/app/feature/site/site.module.ts
+++ src/app/feature/site/site.module.ts
@@ -3,8 +3,14 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
 //Material components
+import { MatIconModule } from '@angular/material/icon';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {MatTableModule} from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { SiteRoutingModule } from './site-routing.module';
 import { SitesComponent } from './components/sites/sites.component';
@@ -19,8 +25,14 @@ import { SiteDetailsComponent } from './components/site-details/site-details.com
     CommonModule,
     SiteRoutingModule,
     HttpClientModule,
+    MatIconModule,
     MatExpansionModule,
-    MatTableModule
+    MatTableModule,
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatInputModule,
+    FlexLayoutModule
   ],
   exports:[
     SitesComponent,
