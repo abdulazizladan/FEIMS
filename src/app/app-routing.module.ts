a7dbf17492dc83ecee1f723d49dd1fda0a2abb80
abdulazizladan <abdulazizladan@gmail.com>
2021-02-09 21:46:41 +0100

    task: remove unwanted components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-//import { LoginComponent } from './auth/components/login/login.component';
-//import { RegisterComponent } from './auth/components/register/register.component';
-//import { ResetPasswordComponent } from './auth/components/reset-password/reset-password.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   {
-    path : 'a',
+    path : '',
     loadChildren : ()=>import('./auth/auth.module').then(mod => mod.AuthModule)
   },
   {
-    path: '',
+    path: 'admin',
     loadChildren : ()=>import('./features/admin/admin.module').then(mod=>mod.AdminModule)
   },
   {
--- src/app/features/admin/admin.module.ts
+++ src/app/features/admin/admin.module.ts
@@ -7,10 +7,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { ChartsModule } from 'ng2-charts';
 
 //Material modules
-import {MatDialogModule} from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatTableModule} from '@angular/material/table';
+import { MatExpansionModule} from '@angular/material/expansion';
+import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
@@ -20,16 +20,10 @@ import { MatCardModule } from '@angular/material/card';
 import { MatSelectModule } from '@angular/material/select';
 import { MatListModule } from '@angular/material/list';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
 import { AdminRoutingModule } from './admin-routing.module';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { NotificationsComponent } from './components/notifications/notifications.component';
-import { UsersComponent } from './components/users/users.component';
-import { SitesComponent } from './components/sites/sites.component';
-import { AddUserDialogComponent } from './components/add-user-dialog/add-user-dialog.component';
-import { AddSiteDialogComponent } from './components/add-site-dialog/add-site-dialog.component';
 import { HomeComponent } from './components/home/home.component';
 import { ReportsComponent } from './components/reports/reports.component';
 import { SettingsComponent } from './components/settings/settings.component';
@@ -39,7 +33,7 @@ import { BuildingComponent } from './components/building/building.component';
 
 
 @NgModule({
-  declarations: [ToolbarComponent, SidenavComponent, DashboardComponent, NotificationsComponent, UsersComponent, SitesComponent, AddUserDialogComponent, AddSiteDialogComponent, HomeComponent, ReportsComponent, SettingsComponent, BuildingsComponent, BuildingSummaryComponent, BuildingComponent],
+  declarations: [ToolbarComponent, SidenavComponent, DashboardComponent, HomeComponent, ReportsComponent, SettingsComponent, BuildingsComponent, BuildingSummaryComponent, BuildingComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
