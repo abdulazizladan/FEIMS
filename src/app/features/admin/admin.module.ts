1d7e0cb114f7c7cddae0d5a3d40214dfe4106f91
abdulazizladan <abdulazizladan@gmail.com>
2021-02-17 12:42:59 +0100

    fix: Replace mat-list with mat-table on home component

--- src/app/features/admin/admin.module.ts
+++ src/app/features/admin/admin.module.ts
@@ -14,7 +14,6 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatDividerModule } from '@angular/material/divider';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
--- src/app/features/admin/components/home/home.component.ts
+++ src/app/features/admin/components/home/home.component.ts
@@ -2,20 +2,63 @@ import { Component, OnInit } from '@angular/core';
 import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
 import { Label, Color } from 'ng2-charts';
 
+
+export interface RepairUpdateData {
+  activity : string;
+  date : string;
+  cost : number;
+}
+
+const REPAIR_UPDATE_DATA : RepairUpdateData[]= [
+  {
+    activity : "Ceiling repair on LAB ABU/LAW/CVL/LT01",
+    date : "01/01/2021",
+    cost : 80000
+  },
+  {
+    activity : "Windows replacement on LAB ABU/LAW/CVL/LT02",
+    date : "01/01/2021",
+    cost : 128000
+  },
+  {
+    activity : "Flush door replacement on LAB ABU/ENG/CVL/LB01",
+    date : "01/01/2021",
+    cost : 750000
+  },
+  {
+    activity : "Saddle housing repair on LAB ABU/ENG/MEC/LB04/LTHE045",
+    date : "01/01/2021",
+    cost : 2500000
+  },
+  {
+    activity : "Window welding at LAB ABU/LAW/CVL/LT05",
+    date : "01/01/2021",
+    cost : 126000
+  }
+]
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
+
 export class HomeComponent implements OnInit {
 
+  dataSource = REPAIR_UPDATE_DATA;
+  displayedColumns: string[] = ['date', 'activity', 'cost'];
+  constructor() {
 
-  constructor() { }
+  }
 
   public barChartOptions = {
     scaleShowVerticalLines: false,
     responsive: true
   };
+
+
+
   public barChartLabels = [
     '2014',
     '2015',
@@ -46,11 +89,12 @@ export class HomeComponent implements OnInit {
         57_000_000
       ],
       label: 'Yearly repair cost',
-      maxBarThickness : 25
+      maxBarThickness : 50
     }
   ];
 
   ngOnInit(): void {
+
   }
 
 }
