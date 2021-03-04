4c25b764681ed4c277861408fd0949aa37c764ba
abdulazizladan <abdulazizladan@gmail.com>
2021-03-04 14:54:16 +0100

    feat: Enable token interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ChartsModule } from 'ng2-charts';
 
 import { CoreModule } from './core/core.module';
 import { AuthService } from './auth/auth.service';
 import { AuthGuard } from './auth/auth.guard'
+import { AuthInterceptor } from './auth/interceptors/auth.interceptor';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -32,7 +33,13 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     NgbModule
   ],
-  providers: [AuthGuard, AuthService],
+  providers: [AuthGuard, AuthService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/auth/interceptors/auth.interceptor.ts
+++ src/app/auth/interceptors/auth.interceptor.ts
@@ -1,15 +1,21 @@
-import { HTTP_INTERCEPTORS, HttpEvent } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
 import { HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
+import { AuthService } from '../auth.service';
 
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor( private auth : AuthService, private injector: Injector) {}
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+  intercept(request: HttpRequest<unknown>, next: HttpHandler){
+    let authService = this.injector.get(AuthService)
+    let tokenizedRequest = request.clone({
+      setHeaders : {
+        Authorization: 'Bearer ${authService.getToken()}'
+      }
+    })
+    return next.handle(tokenizedRequest);
   }
 }
